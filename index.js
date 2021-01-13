5c6ea091e3139b5e56d53ab7157f3938e47cea0a
adrian <revanskyw@gmail.com>
2021-01-13 01:38:03 -0300

    carousel funcionando

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload=function(){
     $('.slider').slick({
     autoplay:true,
-    autoplaySpeed:1500,
+    autoplaySpeed:1000,
     arrows:true,
     prevArrow:'<button type="button" class="slick-prev"></button>',
     nextArrow:'<button type="button" class="slick-next"></button>',
