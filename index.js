9772a917bae2dc9b5d4de605955672d2987f010d
adrian <revanskyw@gmail.com>
2021-01-13 10:40:04 -0300

    modificacion de slider scooter 1

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ window.onload=function(){
     autoplay:true,
     autoplaySpeed:1000,
     arrows:true,
-    prevArrow:'<button type="button" class="slick-prev"></button>',
-    nextArrow:'<button type="button" class="slick-next"></button>',
+    prevArrow:'<button type="button" class="slick-prev"><img src="assets/img/iconos/left-arrow.svg" alt=""></button>',
+    nextArrow:'<button type="button" class="slick-next"><img src="assets/img/iconos/right-arrow.svg" alt=""></button>',
     centerMode:true,
     slidesToShow:2,
     slidesToScroll:3
     });
 
-       $('.slider2').slick({
-            autoplay:true,
-            autoplaySpeed:1000,
-            arrows:true,
-            prevArrow:'<button type="button" class="slick-prev"></button>',
-            nextArrow:'<button type="button" class="slick-next"></button>',
-            centerMode:true,
-            slidesToShow:1,
-            slidesToScroll:1
-            });
+    $('.slider-scooters').slick({
+        autoplay:true,
+        autoplaySpeed:1000,
+        arrows:true,
+        prevArrow:'<button type="button" class="slick-prev"><img src="assets/img/iconos/left-arrow.svg" alt=""></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="assets/img/iconos/right-arrow.svg" alt=""></button>',
+        centerMode:true,
+        slidesToShow:1,
+        slidesToScroll:1
+        });
   };
