996e2fd3bea53b976d32e4ecd0cd357425619ce4
adrian <revanskyw@gmail.com>
2021-01-13 02:37:16 -0300

    carousel movil implementado

--- index.js
+++ index.js
@@ -9,5 +9,15 @@ window.onload=function(){
     slidesToShow:3,
     slidesToScroll:2
     });
+
+       $('.slider2').slick({
+            autoplay:true,
+            autoplaySpeed:1000,
+            arrows:true,
+            prevArrow:'<button type="button" class="slick-prev"></button>',
+            nextArrow:'<button type="button" class="slick-next"></button>',
+            centerMode:true,
+            slidesToShow:1,
+            slidesToScroll:1
+            });
   };
-  
