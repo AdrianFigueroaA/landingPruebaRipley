187b2fd29fa37a21edb21908edc89246e794f971
adrian <revanskyw@gmail.com>
2021-01-13 12:39:13 -0300

    implementacion carousel mobile

--- index.js
+++ index.js
@@ -20,4 +20,16 @@ window.onload=function(){
         slidesToShow:1,
         slidesToScroll:1
         });
+
+    $('.slider-scooters-mobile').slick({
+        autoplay:true,
+        autoplaySpeed:1000,
+        arrows:true,
+        prevArrow:'<button type="button" class="slick-prev"><img src="assets/img/iconos/left-arrow.svg" alt=""></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="assets/img/iconos/right-arrow.svg" alt=""></button>',
+        centerMode:true,
+        slidesToShow:1,
+        slidesToScroll:1
+        });
+
   };
